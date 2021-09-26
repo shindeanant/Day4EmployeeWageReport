9a3e5e645eca1a4f322e9684cc8a234095a7219b
shindeanant <shindeanant1997@gmail.com>
2021-09-26 17:40:53 +0530

    Calculate  Wages Till Given Condition

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -14,6 +14,9 @@ public class EmployeeWage {
 
 	int daysPerMonth = 20;
 
+	int totalHours = 100;
+	int totalDays = 20;
+
 	public void Wlcm() {
 
 		System.out.println("Welcome to Employee Wage Computation Program ");
@@ -62,12 +65,15 @@ public class EmployeeWage {
 			break;
 		}
 		System.out.println();
+		System.out.println();
 
 	}
 
 	public void dailyWage() {
 
 		System.out.println("Daily Employee Wage is: " + wagePerHours * dailyHours);
+
+		System.out.println();
 	}
 
 	public void checkPartAndFullTime() {
@@ -79,6 +85,8 @@ public class EmployeeWage {
 		System.out.println("Part Time Wage = " + PartTimeWage);
 
 		System.out.println("Full Time Wage = " + FullTimeWage);
+
+		System.out.println();
 	}
 
 	public void monthlyWage() {
@@ -88,6 +96,55 @@ public class EmployeeWage {
 		int MonthlyWage = dailyWage * daysPerMonth;
 
 		System.out.println("Monthly wage  = " + MonthlyWage);
+
+		System.out.println();
+	}
+
+	public void CalWagesTillGivenCondition() {
+
+		int empHours;
+		int empWage = 0;
+		int totalEmpWage = 0;
+		int totalWorkingDays = 0;
+		int totalEmpHrs = 0;
+
+		while (totalEmpHrs <= totalHours && totalWorkingDays < totalDays) {
+
+			totalWorkingDays++;
+
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+
+			System.out.println("Random Integers: " + empCheck);
+
+			switch (empCheck) {
+			case 1:
+				empHr = 8;
+				System.out.print("full time");
+				break;
+			case 2:
+				empHr = 4;
+				System.out.print("part time");
+				break;
+			case 0:
+				empHr = 0;
+				System.out.println("absent");
+				break;
+
+			default:
+				empHr = 0;
+				break;
+			}
+
+			empWage = empHr * wagePerHours;
+
+			totalEmpWage = totalEmpWage + empWage;
+			System.out.println("days" + totalWorkingDays + "emphrs" + empHr);
+			System.out.println("wage" + totalEmpWage);
+
+			System.out.println("total wage" + totalEmpWage);
+
+		}
+
 	}
 
 	public static void main(String[] args) {
@@ -105,6 +162,8 @@ public class EmployeeWage {
 		obj.attendanceBySwitchCase();
 
 		obj.monthlyWage();
+
+		obj.CalWagesTillGivenCondition();
 
 	}
 
