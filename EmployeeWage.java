22bffe22984a96978ac7c031fc415b417cc99450
shindeanant <shindeanant1997@gmail.com>
2021-09-26 13:18:54 +0530

    ADD: Welcome Massage

---
+++ EmployeeWage.java
@@ -0,0 +1,18 @@
+package com.day4EmployeeWage;
+
+public class EmployeeWage {
+
+	public void Wlcm() {
+
+		System.out.println("Welcome to Employee Wage Computation Program ");
+	}
+
+	public static void main(String[] args) {
+
+		EmployeeWage obj = new EmployeeWage();
+
+		obj.Wlcm();
+
+	}
+
+}
